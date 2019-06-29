9bd5d0e3df17b49d6858c58e5556b14d9bc81969
Alexey Ponomarev <xlz@ya.ru>
2019-06-29 16:30:12 +0200

    Initial commit

---
+++ src/Main.elm
@@ -0,0 +1,127 @@
+module Main exposing (Model, Msg(..), init, main, update, view)
+
+import Browser
+import Debug exposing (log, toString)
+import Html exposing (Attribute, Html, div, h1, input, label, text)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput)
+import Json.Decode.Extra
+
+
+
+-- MAIN
+
+
+main =
+    Browser.sandbox { init = init, update = update, view = view }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { seconds : String
+    , minutes : String
+    , speed : String
+    }
+
+
+init : Model
+init =
+    { seconds = "0", minutes = "0", speed = "0" }
+
+
+
+-- UPDATE
+
+
+type Msg
+    = ChangeMinutes String
+    | ChangeSeconds String
+    | ChangeSpeed String
+
+
+parseFloat : String -> Float
+parseFloat value =
+    case String.toFloat value of
+        Just parsed ->
+            parsed
+
+        Nothing ->
+            0
+
+
+integralFractional : Float -> ( Float, Float )
+integralFractional value =
+    let
+        truncated =
+            value |> truncate |> toFloat
+    in
+    ( value - truncated, truncated )
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        ChangeMinutes value ->
+            { model
+                | minutes = value
+                , speed =
+                    String.fromFloat
+                        (60
+                            / (parseFloat value
+                                + parseFloat model.seconds
+                                / 60
+                              )
+                        )
+            }
+
+        ChangeSeconds value ->
+            { model
+                | seconds = value
+                , speed =
+                    String.fromFloat
+                        (60
+                            / (parseFloat model.minutes
+                                + parseFloat value
+                                / 60
+                              )
+                        )
+            }
+
+        ChangeSpeed value ->
+            let (fractional, integral) = 60 / (value |> parseFloat) |> integralFractional
+            in
+            { model
+                | speed = value
+                , minutes = integral |> String.fromFloat
+                , seconds = fractional * 60 |> round |> String.fromInt
+            }
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ h1 [] [ text "Tempo" ]
+        , div []
+            [ label []
+                [ text "Minutes"
+                , input [ value model.minutes, onInput ChangeMinutes ] []
+                ]
+            , label []
+                [ text "Seconds"
+                , input [ value model.seconds, onInput ChangeSeconds ] []
+                ]
+            ]
+        , div []
+            [ label []
+                [ text "Speed"
+                , input [ value model.speed, onInput ChangeSpeed ] []
+                ]
+            ]
+        ]
